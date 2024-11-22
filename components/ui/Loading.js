16d9ad33010d53e744f899e4424fbf3d18754fdc
Sutikno Sofjan <sutikno.sofjan@gmail.com>
2024-11-22 09:19:18 +0700

    Update Loading.js

--- components/ui/Loading.js
+++ components/ui/Loading.js
@@ -3,19 +3,10 @@ import { ActivityIndicator, View, StyleSheet } from "react-native";
 
 const Loading = () => {
   return (
-    <View style={styles.loadingContainer}>
+    <View className="flex-1 justify-center items-center bg-white">
       <ActivityIndicator size="large" color="green" />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  loadingContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff", // Sesuaikan warna background sesuai tema
-  },
-});
 
 export default Loading;
